293efa57d22a68808023dc11a92ef88b7451a08d
feraboo <ftabassum@stuy.edu>
2017-08-16 15:41:10 -0400

    Add files via upload

--- final_project/situations.js
+++ final_project/situations.js
@@ -1,3 +1,26 @@
 function dropDown(){
   document.getElementById("myDropdown").classList.toggle("show");
 }
+
+function poison_ivy() {
+  var score = 0;
+  var result;
+  for (i=1; i<6; i++) {
+    if (document.getElementById("correct1").checked){
+      score += 1;
+    // }
+    // if (document.getElementById("correct2").checked){
+    //   score += 1;
+    // }
+    // if (document.getElementById("correct3").checked){
+    //   score += 1;
+    // }
+    // if (document.getElementById("correct4").checked){
+    //   score += 1;
+    // }
+    // if (document.getElementById("correct5").checked){
+    //   score += 1;
+    }}
+    result= "Your chance of surviving is " + (score / 5) * 100 + "%"
+    document.getELementById("result").innerHTML(result);
+  }
